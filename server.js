b0800cb0974dd30c768c378b115768b72799a671
Alexander <vozman@yandex.ru>
2016-12-09 19:30:01 +0000

    release

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
 
- var str=require("strftime");
+
 var express = require('express');
 var app = express();
 
-  var monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
 
-app.get('/:req', function (req, res) {
-  console.log(req.params.req);
-  var dat=new Date(+req.params.req);
-  if(dat.toString()==="Invalid Date")
-  dat=new Date(req.params.req);
-  console.log(dat);
+
+app.get('/', function (req, res) {
   var obj={
-    "unix":null,
-    "natural":null
-  };
-
-
-
-
-  if(dat.toString()!=="Invalid Date"){
-    obj.unix=dat.getTime();
-    obj.natural=(monthNames[dat.getMonth()]+str(" %d,%Y",dat));
+    "ipaddress":null,
+    "language":null,
+    "software":null,
+    "region":null
   }
+  var ip=req.headers['x-forwarded-for'];
+  var lang=req.headers["accept-language"] 
+  
+console.log(req.headers);
+obj.ipaddress=req.headers['x-forwarded-for'];
+obj.language=req.headers["accept-language"].toString().split(",")[0];
+var str=req.headers["user-agent"].toString();
+obj.software=str.slice(str.indexOf("(")+1,str.indexOf(")")) ;
+obj.region=req.headers["x-region"]; 
+
+
+
   res.end(JSON.stringify(obj));
 })
-app.get('/',function(req,res){
-  res.end("Enter value");
-});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
